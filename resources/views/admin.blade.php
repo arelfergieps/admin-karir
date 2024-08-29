c8c88a4619b9495f2f0af6a1f30e462ebc19afbc
selamat maulana <setiawangobit1@gmail.com>
2024-08-29 13:31:41 +0700

    sidebar

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -321,11 +321,11 @@
                 <i class="fa fa-dashboard"></i> <span>Dashboard</span> <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
-                <li class="active"><a href="http://127.0.0.1:8001/karir"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
+                <li class="active"><a href="http://127.0.0.1:8001/karir"><i class="fa fa-circle-o"></i> Karir</a></li>
                 <li><a href="index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
               </ul>
             </li>
-            <li class="treeview">
+            {{-- <li class="treeview">
               <a href="#">
                 <i class="fa fa-files-o"></i>
                 <span>Layout Options</span>
@@ -448,7 +448,7 @@
             <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Important</span></a></li>
             <li><a href="#"><i class="fa fa-circle-o text-yellow"></i> <span>Warning</span></a></li>
             <li><a href="#"><i class="fa fa-circle-o text-aqua"></i> <span>Information</span></a></li>
-          </ul>
+          </ul> --}}
         </section>
         <!-- /.sidebar -->
       </aside>
